75d176397ae1692d6450498f7e346f5799ed4360
Julien Jouganous <julienjouganous@frene-2.genome.mcgill.ca>
2015-12-09 11:11:41 -0500

    iterative discrete Jackknife in progress

--- jackknife.py
+++ jackknife.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import math
 #--------------------------------------------
 # Jackknife extrapolations :
 # used for the moment closure under selection
@@ -64,3 +64,101 @@ def calcJK23(n):
         J[i,ibis+1] = (1+n)*((2+i)*(2+n)*(-4+(i+1)*(3+n))-(5+n)*(n+2*(i+1)*(2+n))*(ibis+1)
                     +(20+9*n+n**2)*(ibis+1)**2)/(3+n)/(4+n)/(5+n)/2
     return J
+
+
+# Discrete iterative Jakknife
+def index(i,n):
+    return int(min(max(math.floor(i*n/(n+1)),1),n-2))
+
+# Compute the matrix to transform Phi_(n+1)->Phi_n
+def matA(n):
+    A = np.zeros((n-1, n))
+    for i in range(0, n-1):
+        A[i, i] = (n-i)/(n+1)
+        A[i, i+1] = (i+2)/(n+1)
+    return A
+
+# Disctrete JK using Cramer formula
+def calcJKD(n,v1,v2):
+    J = np.zeros((n, n-1))
+    A = matA(n)
+    # cas général
+    for i in range(1,n-1):
+        a1 = np.longdouble(v1[i]*A[i-1,i]+v1[i-1]*A[i-1,i-1])
+        a2 = np.longdouble(v1[i]*A[i,i]+v1[i+1]*A[i,i+1])
+        a3 = np.longdouble(v2[i]*A[i-1,i]+v2[i-1]*A[i-1,i-1])
+        a4 = np.longdouble(v2[i]*A[i,i]+v2[i+1]*A[i,i+1])
+        J[i,i-1] = np.longdouble((v1[i]*a4-v2[i]*a2)/(a1*a4-a3*a2))
+        J[i,i] = np.longdouble((a1*v2[i]-a3*v1[i])/(a1*a4-a3*a2))
+    # case i=0
+    a1 = np.longdouble(v1[0]*A[0,0]+v1[1]*A[0,1])
+    a2 = np.longdouble(v1[1]*A[1,1]+v1[2]*A[1,2])
+    a3 = np.longdouble(v2[0]*A[0,0]+v2[1]*A[0,1])
+    a4 = np.longdouble(v2[1]*A[1,1]+v2[2]*A[1,2])
+    J[0,0] = np.longdouble((v1[0]*a4-v2[0]*a2)/(a1*a4-a3*a2))
+    J[0,1] = np.longdouble((a1*v2[0]-a3*v1[0])/(a1*a4-a3*a2))
+
+    # case i=n-1
+    a1 = np.longdouble(v1[n-1]*A[n-2,n-1]+v1[n-2]*A[n-2,n-2])
+    a2 = np.longdouble(v1[n-2]*A[n-3,n-2]+v1[n-3]*A[n-3,n-3])
+    a3 = np.longdouble(v2[n-1]*A[n-2,n-1]+v2[n-2]*A[n-2,n-2])
+    a4 = np.longdouble(v2[n-2]*A[n-3,n-2]+v2[n-3]*A[n-3,n-3])
+    J[n-1,n-2] = np.longdouble((v1[n-1]*a4-v2[n-1]*a2)/(a1*a4-a3*a2))
+    J[n-1,n-3] = np.longdouble((a1*v2[n-1]-a3*v1[n-1])/(a1*a4-a3*a2))
+    return J
+
+# with np.linajg.solve
+def calcJKD2(n,v1,v2):
+    J = np.zeros((n, n-1))
+    A = matA(n)
+    # cas général
+    for i in range(1,n-1):
+        m = np.array([[v1[i]*A[i-1,i]+v1[i-1]*A[i-1,i-1], v1[i]*A[i,i]+v1[i+1]*A[i,i+1]], [v2[i]*A[i-1,i]+v2[i-1]*A[i-1,i-1], v2[i]*A[i,i]+v2[i+1]*A[i,i+1]]])
+        j = np.linalg.solve(m,np.array([v1[i], v2[i]]))
+        J[i,i-1] = j[0]
+        J[i,i] = j[1]
+    # cas i=0
+    m = np.array([[v1[0]*A[0,0]+v1[1]*A[0,1], v1[1]*A[1,1]+v1[2]*A[1,2]], [v2[0]*A[0,0]+v2[1]*A[0,1], v2[1]*A[1,1]+v2[2]*A[1,2]]])
+    j = np.linalg.solve(m,np.array([v1[0], v2[0]]))
+    J[0,0] = j[0]
+    J[0,1] = j[1]
+
+    # cas i=n-1
+    m = np.array([[v1[n-1]*A[n-2,n-1]+v1[n-2]*A[n-2,n-2], v1[n-2]*A[n-3,n-2]+v1[n-3]*A[n-3,n-3]], [v2[n-1]*A[n-2,n-1]+v2[n-2]*A[n-2,n-2], v2[n-2]*A[n-3,n-2]+v2[n-3]*A[n-3,n-3]]])
+    j = np.linalg.solve(m,np.array([v1[n-1], v2[n-1]]))
+    J[n-1,n-2] = j[0]
+    J[n-1,n-3] = j[1]
+    return J
+
+# Disctrete JK using Cramer formula
+def calcJKD3(n,v1,v2):
+    J = np.zeros((n, n-1))
+    A = matA(n)
+    # cas général
+    for i in range(1,n-1):
+        a1 = v1[i]*A[i-1,i]+v1[i-1]*A[i-1,i-1]
+        a2 = v1[i]*A[i,i]+v1[i+1]*A[i,i+1]
+        a3 = v2[i]*A[i-1,i]+v2[i-1]*A[i-1,i-1]
+        a4 = v2[i]*A[i,i]+v2[i+1]*A[i,i+1]
+        J[i,i-1] = (v1[i]*a4-v2[i]*a2)/(a1*a4-a3*a2)
+        J[i,i] = (a1*v2[i]-a3*v1[i])/(a1*a4-a3*a2)
+
+    # case i=0
+    a1 = v1[0]*A[0,0]+v1[1]*A[0,1]
+    a2 = v1[1]*A[1,1]+v1[2]*A[1,2]
+    a3 = v2[0]*A[0,0]+v2[1]*A[0,1]
+    a4 = v2[1]*A[1,1]+v2[2]*A[1,2]
+    J[0,0] = (v1[0]*a4-v2[0]*a2)/(a1*a4-a3*a2)
+    J[0,1] = (a1*v2[0]-a3*v1[0])/(a1*a4-a3*a2)
+
+    # case i=n-1
+    a1 = v1[n-1]*A[n-2,n-1]+v1[n-2]*A[n-2,n-2]
+    a2 = v1[n-2]*A[n-3,n-2]+v1[n-3]*A[n-3,n-3]
+    a3 = v2[n-1]*A[n-2,n-1]+v2[n-2]*A[n-2,n-2]
+    a4 = v2[n-2]*A[n-3,n-2]+v2[n-3]*A[n-3,n-3]
+    J[n-1,n-2] = (v1[n-1]*a4-v2[n-1]*a2)/(a1*a4-a3*a2)
+    J[n-1,n-3] = (a1*v2[n-1]-a3*v1[n-1])/(a1*a4-a3*a2)
+    return J
+
+
+
