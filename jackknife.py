672de9d5224f5624f28aa43da12ed2dc163277ba
julien.jouganous <julien.jouganous@gmail.com>
2016-01-07 16:13:24 -0500

    simplification in the index calculation

--- jackknife.py
+++ jackknife.py
@@ -9,7 +9,7 @@ import math
 
 # The choice i' in n samples that best approximates the frequency of \i/(n + 1) is i*n / (n + 1)
 def index_bis(i,n):
-    return int(max(round(i*n/(n+1)),2))
+    return int(min(max(round(i*n/(n+1)),2),n-2))
 
 # Compute the order 2 Jackknife extrapolation coefficients for 1 jump (Phi_n -> Phi_(n+1))
 def calcJK12(n):
@@ -28,7 +28,6 @@ def calcJK13(n):
     J = np.zeros((n, n-1))
     for i in range(0, n):
         ibis = index_bis(i+1,n)-1
-        if (i == n-1): ibis -= 1
         J[i,ibis] = -(1+n)*((2+i)*(2+n)*(-6-n+(i+1)*(3+n))-2*(4+n)*(-1+(i+1)*(2+n))*(ibis+1)
                   +(12+7*n+n**2)*(ibis+1)**2)/(2.0+n)/(3.0+n)/(4.0+n)
         J[i,ibis-1] = (1+n)*(4+(1+i)**2*(6+5*n+n**2)-(i+1)*(14+9*n+n**2)-(4+n)*(-5-n+2*(i+1)*(2+n))*(ibis+1)
