45cb3ecde79832108745fe9d2f3751956cf2bc09
Will Long <wlong799@gmail.com>
2016-08-06 15:00:27 -0400

    Improvements to plot appearance, including a scale
    Added gridlines to y-axis instead of using ticks, to make population
    sizes clearer. Also spaced migration arrows evenly across time periods
    and gave the option to not include them in the plot at all. Added a
    scale to show population size in plotted model, and gave option to use
    relative or absolute scales.

--- moments/ModelPlot.py
+++ moments/ModelPlot.py
@@ -18,10 +18,11 @@ LinearSystem.steady_state
 Manips.split_{1D_to_2D,2D_to_3D_2,2D_to_3D_1,3D_to_4D_3,4D_to_5D_4}
 Spectrum_mod.Spectrum.integrate
 """
-import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.offsetbox as mbox
 import matplotlib.patches as mpatches
+import matplotlib.pyplot as plt
 import matplotlib.ticker as mticker
+import numpy as np
 
 ## USER FUNCTIONS ##
 def generate_model(model_func, params, ns, precision=100):
@@ -54,17 +55,29 @@ def generate_model(model_func, params, ns, precision=100):
         vstart += tp1.framesizes[pop_index]
     return model
 
-def plot_model(model, fig_title="Demographic Model", fig_bg_color='#657b83', 
-               plot_bg_color='#839496', pop_color='#268bd2', 
-               arrow_color='#073642', arrow_scale=0.01, text_color='#002b36',
-               save_file = None, pop_labels=None):
+def plot_model(model, save_file=None, pop_labels=None, nref=None, 
+               fig_title="Demographic Model", fig_bg_color='#ffffff', 
+               plot_bg_color='#93a1a1', gridline_color='#586e75', 
+               text_color='#002b36', pop_color='#268bd2', 
+               draw_migrations=True, arrow_color='#073642', arrow_scale=0.01):         
     """
     Plots a demographic model based on information contained within a _ModelInfo
     object. Returns the matplotlib Figure object that was created. See the
-    matplotlib documentation for valid values for the color options.
+    matplotlib documentation for valid entries for the color parameters.
 
     model : A _ModelInfo object created using generate_model().
 
+    save_file : If not None, the figure will be saved to this location.
+
+    nref : If specified, this will update the time and population size labels to
+           use units based on an ancestral population size of nref. See the
+           documentation for details.
+
+    pop_labels : If not None, should be a list of strings of the same length as
+                 the total number of final populations in the model. The string
+                 at index i should be the name of the population along axis i in
+                 the model's SFS.
+    
     fig_title : Title of the figure.
 
     fig_bg_color : Background color of figure (i.e. border surrounding the
@@ -72,29 +85,29 @@ def plot_model(model, fig_title="Demographic Model", fig_bg_color='#657b83',
 
     plot_bg_color : Background color of the actual plot area.
 
+    gridline_color : Color of the plot gridlines.
+
+    text_color : Color of text in the figure.
+
     pop_color : Color of the populations.
+
+    draw_migrations : If False, migration arrows will not be drawn.
 
     arrow_color : Color of the arrows showing migrations between populations.
 
     arrow_scale : Float to control the size of the migration arrows.
-    
-    text_color : Color of text in the figure.
-
-    save_file : If not None, the figure will be saved to this location.
-
-    pop_labels : If not None, should be a list of strings of the same length as
-                 the total number of final populations in the model. The string
-                 at index i should be the name of the population along axis i in
-                 the model's SFS.
     """
     # Set up basics of plot
     fig = plt.figure(facecolor=fig_bg_color)
     ax = fig.add_subplot(111)
     ax.set_axis_bgcolor(plot_bg_color)
     ax.set_title(fig_title, color=text_color,fontsize=24)
-    ax.set_xlabel("Time (Genetic Units)", color=text_color, fontsize=16)
-    ax.set_ylabel("Relative Population Sizes", color=text_color, fontsize=16)
-
+    if nref:
+        ax.set_xlabel("Time (Generations)", color=text_color, fontsize=16)
+        ax.set_ylabel("Population Sizes", color=text_color, fontsize=16)
+    else:
+        ax.set_xlabel("Time (Genetic Units)", color=text_color, fontsize=16)
+        ax.set_ylabel("Relative Population Sizes", color=text_color, fontsize=16)
     # Determine various max values for proper scaling within the model
     xmax = model.tp_list[-1].time[-1]
     ymax = sum(model.tp_list[0].framesizes)
@@ -105,25 +118,45 @@ def plot_model(model, fig_title="Demographic Model", fig_bg_color='#657b83',
         mig = np.amax(tp.migrations)
         mig_max = mig_max if mig_max > mig else mig
 
-    # Configure minor ticks
-    ax.xaxis.set_minor_locator(mticker.NullLocator())
-    ax.yaxis.set_minor_locator(mticker.FixedLocator(np.arange(ymax)))
-    # Major tick on time axis only for change in time periods
-    xticks = [tp.time[0] for tp in model.tp_list]
-    xticks.append(xmax)
-    ax.xaxis.set_major_locator(mticker.FixedLocator(xticks))
-    
-    # Configure axis tick/label colors
+    # Configure axis border colors
     ax.spines['top'].set_color(text_color)
     ax.spines['right'].set_color(text_color)
     ax.spines['bottom'].set_color(text_color)
     ax.spines['left'].set_color(text_color)
-    ax.tick_params(which='both', colors=text_color, top=False, right=False,
-                   labelsize=12)
     
+    # Configure ticks along x-axis (time)
+    xticks = [tp.time[0] for tp in model.tp_list]
+    xticks.append(xmax)
+    ax.xaxis.set_major_locator(mticker.FixedLocator(xticks))
+    ax.xaxis.set_minor_locator(mticker.NullLocator())
+    ax.tick_params(which='both', axis='x', labelcolor=text_color,
+                   labelsize=12, top=False)
+    # If nref is given use the appropriate time units (2*nref generations)
+    if nref:
+        ax.set_xticklabels([str(2*nref*x) for x in xticks])
+
+    # Configure gridlines along y-axis (population size)
+    ax.yaxis.set_major_locator(mticker.FixedLocator(np.arange(ymax)))
+    ax.yaxis.set_minor_locator(mticker.NullLocator())
+    ax.grid(b=True, which='major', axis='y', color=gridline_color)
+    ax.tick_params(which='both', axis='y', colors='none')
+
+    # Add population size scale
+    bar = mbox.AuxTransformBox(ax.transData)
+    bar.add_artist(mpatches.Rectangle((0,0), 0, 1, edgecolor=text_color,
+                                      facecolor='none'))
+    label = mbox.TextArea(str(nref) if nref else "Nref")
+    label.get_children()[0].set_color(text_color)
+    bar = mbox.HPacker(children=[label, bar], pad=0, sep=2,
+                       align="center")
+    scalebar = mbox.AnchoredOffsetbox(2, pad=0.25, borderpad=0.25, child=bar,
+                                      frameon=False)
+    ax.add_artist(scalebar)
+
     # Iterate through time periods and populations to draw everything
     for tp_index, tp in enumerate(model.tp_list):
-        # Keep track of migrations plotted so arrows don't overlap
+        # Keep track of migrations to evenly space arrows across time period
+        total_migrations = np.count_nonzero(tp.migrations)
         num_migrations = 0
         for pop_index in range(len(tp.popsizes)):
             # Draw current population
@@ -167,14 +200,15 @@ def plot_model(model, fig_title="Demographic Model", fig_bg_color='#657b83',
                                 edgecolor=pop_color)
 
             # Draw migrations if necessary
-            if tp.migrations is not None:
+            if draw_migrations and tp.migrations is not None:
                 # Iterate through migrations for current population 
                 for mig_index, mig_val in enumerate(tp.migrations[pop_index]):
                     # If no migration, continue
                     if mig_val == 0:
                         continue
                     num_migrations += 1
-                    offset = num_migrations*10
+                    offset = int(tp.precision * num_migrations / 
+                             (total_migrations + 1.0))
                     x = tp.time[offset]
                     dx = 0
                     # Determine which sides of populations are closest
@@ -206,7 +240,7 @@ def plot_model(model, fig_title="Demographic Model", fig_bg_color='#657b83',
         ax2.yaxis.set_major_locator(mticker.FixedLocator(yticks))
         ax2.set_yticklabels(pop_labels)
         ax2.tick_params(which='both', color='none', labelcolor=text_color,
-                        labelsize=16)
+                        labelsize=16, left=False, top=False, right=False)
         ax2.spines['top'].set_color(text_color)
         ax2.spines['left'].set_color(text_color)
         ax2.spines['right'].set_color(text_color)
@@ -218,6 +252,8 @@ def plot_model(model, fig_title="Demographic Model", fig_bg_color='#657b83',
         plt.savefig(save_file, dpi=200, facecolor=fig_bg_color)
     else:
         plt.show(fig)
+
+    return fig
        
 
 ## IMPLEMENTATION FUNCTIONS ##
@@ -453,3 +489,46 @@ class _ModelInfo():
             else:
                 self.determine_drawinfo(tp_index+1, desc, 
                                         (tp.time[-1], origin[1]), direc)
+
+
+# The following code was adapted from scalebars.py, by dmeliza, located here on
+# GitHub: https://gist.github.com/dmeliza/3251476. scalebars.py is licensed with
+# the Python Software Foundation license (http://docs.python.org/license.html).
+class _AnchoredScaleBar(mbox.AnchoredOffsetbox):
+    def __init__(self, transform, sizex=0, sizey=0, labelx=None, labely=None, 
+                 loc=4, pad=0.1, borderpad=0.1, sep=2, prop=None, **kwargs):
+        """
+        Draw a horizontal and/or vertical bar with the size in data coordinates
+        of the give axes. A label will be drawn underneath (center-aligned).
+        
+        transform : Coordinate frame to use (typically axes.transData)
+        
+        sizex,sizey: Width of the x and y bars, in data units. 0 to omit.
+
+        labelx,labely : Labels for the x and y  bars. None to omit.
+
+        loc : Position in containing axes.
+
+        pad,borderpad : Padding, in fraction of the legend font size (or prop).
+
+        sep : Separation between labels and bars in points.
+        
+        **kwargs : Additional arguments passed to base class constructor.
+        """
+        bars = mbox.AuxTransformBox(transform)
+        if sizex:
+            bars.add_artist(mpatches.Rectangle((0,0), sizex, 0, fc="none"))
+        if sizey:
+            bars.add_artist(mpatches.Rectangle((0,0), 0, sizey, fc="none"))
+
+        if sizex and labelx:
+            bars = mbox.VPacker(children=[bars, mbox.TextArea(labelx, 
+                                minimumdescent=False)], pad=0, sep=sep,
+                                align="center")
+        if sizey and labely:
+            bars = mbox.HPacker(children=[mbox.TextArea(labely), bars], pad=0,
+                                sep=sep, align="center")
+
+        mbox.AnchoredOffsetbox.__init__(self, loc, pad=pad, borderpad=borderpad,
+                                        child=bars, prop=prop, frameon=False, 
+                                        **kwargs)
