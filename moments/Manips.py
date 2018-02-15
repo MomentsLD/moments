fix to conserve masking of corners when splitting spectrum

moments/Manips.py
@@ -27,6 +27,13 @@ def split_1D_to_2D(sp, n1, n2):
     
     Returns a new 2D spectrum
     """
+    # Check if corners masked - if they are, keep split corners masked
+    # If they are unmasked, keep split spectrum corners unmasked
+    if sp.mask[0] == True and sp.mask[-1] == True:
+        masked_corners = True
+    else:
+        masked_corners = False
+    
     # Update ModelPlot if necessary
     model = ModelPlot._get_model()
     if model is not None:
@@ -45,12 +52,8 @@ def split_1D_to_2D(sp, n1, n2):
         for j in range(n2 + 1):
             data_2D[i, j] = sp[i + j] * misc.comb(n1, i) * misc.comb(n2, j)  \
                             / misc.comb(n1 + n2, i + j)
-    # if all entries unmasked, keep them all unmaksed in the split spectrum
-    # care about this for finite genome model
-    if np.any(sp.mask) == False:
-        return Spectrum_mod.Spectrum(data_2D, mask_corners=False)
-    else:
-        return Spectrum_mod.Spectrum(data_2D)
+
+    return Spectrum_mod.Spectrum(data_2D, mask_corners=masked_corners)
 
 def split_2D_to_3D_2(sp, n2new, n3):
     """
@@ -65,6 +68,13 @@ def split_2D_to_3D_2(sp, n2new, n3):
 
     Returns a new 3D spectrum
     """
+    # Check if corners masked - if they are, keep split corners masked
+    # If they are unmasked, keep split spectrum corners unmasked
+    if sp.mask[0,0] == True and sp.mask[-1,-1] == True:
+        masked_corners = True
+    else:
+        masked_corners = False
+
     # Update ModelPlot if necessary
     model = ModelPlot._get_model()
     if model is not None:
@@ -85,12 +95,8 @@ def split_2D_to_3D_2(sp, n2new, n3):
         for j in range(n3 + 1):
             data_3D[:, i, j] = sp[:, i + j] * misc.comb(n2new, i) * misc.comb(n3, j)  \
                                / misc.comb(n2new + n3, i + j)
-    # if all entries unmasked, keep them all unmaksed in the split spectrum
-    # care about this for finite genome model
-    if np.any(sp.mask) == False:
-        return Spectrum_mod.Spectrum(data_3D, mask_corners=False)
-    else:
-        return Spectrum_mod.Spectrum(data_3D)
+
+    return Spectrum_mod.Spectrum(data_2D, mask_corners=masked_corners)
 
 def split_2D_to_3D_1(sp, n1new, n3):
     """
@@ -105,6 +111,13 @@ def split_2D_to_3D_1(sp, n1new, n3):
     
     Returns a new 3D spectrum
     """
+    # Check if corners masked - if they are, keep split corners masked
+    # If they are unmasked, keep split spectrum corners unmasked
+    if sp.mask[0,0,0] == True and sp.mask[-1,-1,-1] == True:
+        masked_corners = True
+    else:
+        masked_corners = False
+
     # Update ModelPlot if necessary
     model = ModelPlot._get_model()
     if model is not None:
@@ -125,12 +138,8 @@ def split_2D_to_3D_1(sp, n1new, n3):
         for j in range(n3 + 1):
             data_3D[i, :, j] = sp[i + j, :] * misc.comb(n1new, i) * misc.comb(n3, j)  \
                                / misc.comb(n1new + n3, i + j)
-    # if all entries unmasked, keep them all unmaksed in the split spectrum
-    # care about this for finite genome model
-    if np.any(sp.mask) == False:
-        return Spectrum_mod.Spectrum(data_3D, mask_corners=False)
-    else:
-        return Spectrum_mod.Spectrum(data_3D)
+
+    return Spectrum_mod.Spectrum(data_2D, mask_corners=masked_corners)
 
 
 def split_3D_to_4D_3(sp, n3new, n4):
@@ -146,6 +155,13 @@ def split_3D_to_4D_3(sp, n3new, n4):
    
     Returns a new 4D spectrum
     """ 
+    # Check if corners masked - if they are, keep split corners masked
+    # If they are unmasked, keep split spectrum corners unmasked
+    if sp.mask[0,0,0,0,0] == True and sp.mask[-1,-1,-1,-1,-1] == True:
+        masked_corners = True
+    else:
+        masked_corners = False
+
     # Update ModelPlot if necessary
     model = ModelPlot._get_model()
     if model is not None:
@@ -167,12 +183,8 @@ def split_3D_to_4D_3(sp, n3new, n4):
         for j in range(n4 + 1):
             data_4D[:, :, i, j] = sp[:, :, i + j] * misc.comb(n3new, i) * misc.comb(n4, j)  \
                                   / misc.comb(n3new + n4, i + j)
-    # if all entries unmasked, keep them all unmaksed in the split spectrum
-    # care about this for finite genome model
-    if np.any(sp.mask) == False:
-        return Spectrum_mod.Spectrum(data_4D, mask_corners=False)
-    else:
-        return Spectrum_mod.Spectrum(data_4D)
+
+    return Spectrum_mod.Spectrum(data_2D, mask_corners=masked_corners)
 
 def split_4D_to_5D_4(sp, n4new, n5):
     """
@@ -187,6 +199,13 @@ def split_4D_to_5D_4(sp, n4new, n5):
     
     Returns a new 5D spectrum
     """
+    # Check if corners masked - if they are, keep split corners masked
+    # If they are unmasked, keep split spectrum corners unmasked
+    if sp.mask[0,0,0,0,0] == True and sp.mask[-1,-1,-1,-1,-1] == True:
+        masked_corners = True
+    else:
+        masked_corners = False
+
     # Update ModelPlot if necessary
     model = ModelPlot._get_model()
     if model is not None:
@@ -209,12 +228,8 @@ def split_4D_to_5D_4(sp, n4new, n5):
         for j in range(n5 + 1):
             data_5D[:, :, :, i, j] = sp[:, :, :, i + j] * misc.comb(n4new, i)  \
                                      * misc.comb(n5, j) / misc.comb(n4new + n5, i + j)
-    # if all entries unmasked, keep them all unmaksed in the split spectrum
-    # care about this for finite genome model
-    if np.any(sp.mask) == False:
-        return Spectrum_mod.Spectrum(data_5D, mask_corners=False)
-    else:
-        return Spectrum_mod.Spectrum(data_5D)
+
+    return Spectrum_mod.Spectrum(data_2D, mask_corners=masked_corners)
 
 def split_4D_to_5D_3(sp, n3new, n4):
     """