071840f76a907e145ead0810b92ef66314046639
Aaron Ragsdale <aragsdale@math.arizona.edu>
2018-03-12 18:47:27 -0400

    Add to and from file for LD stats objects

--- moments/LD/LDstats_mod.py
+++ moments/LD/LDstats_mod.py
@@ -14,24 +14,25 @@ class LDstats(numpy.ma.masked_array):
     """
     Represents linkage disequilibrium statistics (LDstats).
     
-    LDstats are represented as an array of statistics over two locus pairs for a given 
-    recombination distance.
+    LDstats are represented as an array of statistics over two locus pairs for 
+    a given recombination distance.
     
     The constructor has the format:
         y = moments.LD.LDstats(data, mask, order, num_pops)
         
         data: The list of statistics
         mask: An optional array of the same size. 'True' entries in this array
-              are masked in LDstats. These represent missing data, invalid entries,
-              or entries we wish to ignore when performing inference using LD statistics.
-        order: The order of the system for D (D^n). For more than one population, we only
-               have order 2 statistics.
-        num_pops: Number of populations. For one population, higher order statistics may be 
-                  computed.
+              are masked in LDstats. These represent missing data, invalid 
+              entries, or entries we wish to ignore when performing inference 
+              using LD statistics.
+        order: The order of the system for D (D^n). For more than one 
+               population, we only have order 2 statistics.
+        num_pops: Number of populations. For one population, higher order 
+                  statistics may be computed.
     """
     def __new__(subtype, data, mask=numpy.ma.nomask, order=2, num_pops=1,
                 dtype=float, copy=True, fill_value=numpy.nan, keep_mask=True,
-                shrink=True):
+                shrink=True, pop_ids=None):
         if num_pops == None:
             raise ValueError('Specify number of populations as num_pops=.')
         
@@ -46,8 +47,8 @@ class LDstats(numpy.ma.masked_array):
             mask = numpy.ma.make_mask_none(data.shape)
 
         subarr = numpy.ma.masked_array(data, mask=mask, dtype=dtype, copy=copy,
-                                       fill_value=fill_value, keep_mask=keep_mask,
-                                       shrink=True)
+                                    fill_value=fill_value, keep_mask=keep_mask,
+                                    shrink=True)
         
         subarr = subarr.view(subtype)
         if hasattr(data, 'order'):
@@ -60,6 +61,10 @@ class LDstats(numpy.ma.masked_array):
         else:
             subarr.num_pops = num_pops
         
+        if hasattr(data, 'pop_ids'):
+            subarr.pop_ids = data.pop_ids
+        else:
+            subarr.pop_ids = pop_ids
         return subarr
 
     # See http://www.scipy.org/Subclasses for information on the
@@ -69,7 +74,7 @@ class LDstats(numpy.ma.masked_array):
     # Also, see http://docs.scipy.org/doc/numpy/user/basics.subclassing.html
     #
     # We need these methods to ensure extra attributes get copied along when
-    # we do arithmetic on the FS.
+    # we do arithmetic on the LD stats.
     def __array_finalize__(self, obj):
         if obj is None: 
             return
@@ -116,15 +121,15 @@ class LDstats(numpy.ma.masked_array):
         y: LDstats object for 
         pop_to_split: index of population to split
         So split(y, 2, 1) would split 2 to 3 pops, splitting pop1
-        If the populations are labeled [pop1, pop2], with the new pop pop_new, the 
-        output statistics would have population order [pop1, pop2, pop_new]
+        If the populations are labeled [pop1, pop2], with the new pop pop_new, 
+        the output statistics would have population order [pop1, pop2, pop_new]
         New population always appended on end. 
         """
         mom_list_from = Numerics.moment_list(self.num_pops)
         mom_list_to = Numerics.moment_list(self.num_pops+1)
         y_from = self.data
         y_new = np.ones(len(mom_list_to)+1)
-        # dictionary to point where moments in mom_list_to come from in mom_list_from
+        # dictionary point where stats in mom_list_to come from in mom_list_from
         points = {}
         for mom_to in mom_list_to:
             if mom_to in mom_list_from:
@@ -156,12 +161,127 @@ class LDstats(numpy.ma.masked_array):
     # Make from_file a static method, so we can use it without an instance.
     @staticmethod
     def from_file(fid, return_comments=False):
-        pass
+        """
+        Read LD statistics from file
+        
+        fid: string with file name to read from or an open file object.
+        return_comments: If true, the return value is (y, comments), where
+                         comments is a list of strings containing the comments
+                         from the file (without #'s).
+        """
+        newfile = False
+        # Try to read from fid. If we can't, assume it's something that we can
+        # use to open a file.
+        if not hasattr(fid, 'read'):
+            newfile = True
+            fid = file(fid, 'r')
+        
+        line = fid.readline()
+        # Strip out the comments
+        comments = []
+        while line.startswith('#'):
+            comments.append(line[1:].strip())
+            line = fid.readline()
+        
+        # Read the order, model type of the data
+        line_spl = line.split()
+        order = int(line_spl[0])
+        model_type = line_spl[1]
+        num_pops = int(line_spl[2])
+        if len(line_spl) > 3:
+            # get the pop_ids
+            pop_ids = line_spl[3:]
+        else:
+            pop_ids = None
+        
+        data = numpy.fromstring(fid.readline().strip(), sep=' ')
+
+        maskline = fid.readline().strip()
+        mask = numpy.fromstring(maskline, sep=' ')
+
+        # If we opened a new file, clean it up.
+        if newfile:
+            fid.close()
+
+        y = LDstats(data, mask, order=order, num_pops=num_pops, pop_ids=pop_ids)
+
+        if not return_comments:
+            return y
+        else:
+            return y,comments
+        
+        
     
-    def to_file():
-        pass
+    def to_file(self, fid, precision=16, comment_lines=[]):
+        """
+        Write LD statistics to file.
+        
+        fid: string with file name to write to or an open file object.
+        precision: precision with which to write out entries of the LD stats. 
+                   (They are formated via %.<p>g, where <p> is the precision.)
+        comment lines: list of strings to be used as comment lines in the header
+                       of the output file.
+        foldmaskinfo: If False, folding and mask and population label
+                      information will not be saved. 
 
+        The file format is:
+            # Any number of comment lines beginning with a '#'
+            A single line containing an integer giving the order of LD 
+              statistics. So this line would be '4' if we have modeled
+              the order D^4 system.
+            On the *same line*, the string 'onepop' or 'multipop' denoting 
+              whether we have a single or multipop object.
+            On the *same line*, number of populations (must be '1' if we have
+              written 'onepop', and can be any integer for 'multipop').
+            On the *same line*, optional strings each containing the population
+              labels in quotes separated by spaces, e.g. "pop 1" "pop 2"
+            A single line giving the array elements. The order of elements is 
+              given by order in Numerics.moment_names...
+            A single line giving the elements of the mask in the same order as
+              the data line. '1' indicates masked, '0' indicates unmasked.
+        """
+        # Open the file object.
+        newfile = False
+        if not hasattr(fid, 'write'):
+            newfile = True
+            fid = file(fid, 'w')
 
+        # Write comments
+        for line in comment_lines:
+            fid.write('# ')
+            fid.write(line.strip())
+            fid.write(os.linesep)
+
+        # Write out the order of the LDstats
+        fid.write('%i ' % self.order)
+        
+        # Write out model type (onepop or multipop) and number of populations
+        if len(self.data) == 5:
+            fid.write('onepop %i' % self.num_pops)
+        else:
+            fid.write('multipop %i' % self.num_pops)
+        
+        if self.pop_ids is not None:
+            for label in self.pop_ids:
+                fid.write(' "%s"' % label)
+        
+        fid.write(os.linesep)
+
+        # Write the data to the file
+        self.data.tofile(fid, ' ', '%%.%ig' % precision)
+        fid.write(os.linesep)
+
+        # Write the mask to the file
+        numpy.asarray(self.mask,int).tofile(fid, ' ')
+        fid.write(os.linesep)
+
+        # Close file
+        if newfile:
+            fid.close()
+
+    tofile = to_file
+        
+    
     # Ensures that when arithmetic is done with LDstats objects,
     # attributes are preserved. For details, see similar code in
     # moments.Spectrum_mod
@@ -195,22 +315,23 @@ def %(method)s(self, other):
 """ % {'method':method})
     
     
-    def integrate(self, nu, tf, dt=0.001, rho=None, theta=0.0008, ism=False, m=None):
+    def integrate(self, nu, tf, dt=0.001, rho=None, theta=0.0008, ism=False, 
+                  m=None):
         """
-        Integrates the LD statistics forward in time. The tricky part is combining
-        single population and multi-population integration routines. 
+        Integrates the LD statistics forward in time. The tricky part is 
+        combining single population and multi-population integration routines. 
         11/30: For now, this is just single population
         nu: relative population size, may be a function of time
         tf: total time to integrate
         dt: integration timestep
-        rho: can be a single recombination rate or list of recombination rates (in which 
-             case we are integrating a list of LD stats for each rate)
+        rho: can be a single recombination rate or list of recombination rates 
+             (in which case we are integrating a list of LD stats for each rate)
         theta: per base population-scaled mutation rate (4N*mu)
-        ism: if True, we use the infinite sites model, otherwise we use a reversible
-             mutation model (equal forward and reverse mutation rates)
-             Note that the ism model is only implemented in the single population
-             model with basis that includes p(1-p)q(1-q) term. Not possible to 
-             include in the multipopulation model.
+        ism: if True, we use the infinite sites model, otherwise we use a 
+             reversible mutation model (equal forward and reverse mutation 
+             rates) Note that the ism model is only implemented in the single 
+             population model with basis that includes p(1-p)q(1-q) term. Not 
+             possible to include in the multipopulation model.
         """
         order = self.order
         num_pops = self.num_pops
@@ -224,12 +345,14 @@ def %(method)s(self, other):
         
         if num_pops == 1 and len(self.data) == 5:
             # this is the system [D^2, Dz, pi2, pi, 1]
-            self.data[:] = Numerics.integrate(self.data, tf, rho=rho, nu=nu, theta=theta,
-                                            order=order, dt=dt, ism=ism)
+            self.data[:] = Numerics.integrate(self.data, tf, rho=rho, nu=nu, 
+                                    theta=theta, order=order, dt=dt, ism=ism)
         else:
-            # this is the system [D^2, Dz, z^2, z_p, z^q, 1] and multipop with same basis
+            # this is the system [D^2, Dz, z^2, z_p, z^q, 1] 
+            # and multipop with same basis
             self.data[:] = Numerics.integrate_multipop(self.data, nu, tf, dt=dt,
-                                            rho=rho, theta=theta, m=m, num_pops=self.num_pops)
+                                    rho=rho, theta=theta, m=m, 
+                                    num_pops=self.num_pops)
 
 
 # Allow LDstats objects to be pickled.
