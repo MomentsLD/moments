275434b8d72c3d46bdded8f01dc1988c318791d0
Aaron Ragsdale <aragsdale@math.arizona.edu>
2019-02-20 11:25:28 -0500

    add inference

---
+++ moments/LD/Inference.py
@@ -0,0 +1,384 @@
+import numpy as np
+import math
+import os,sys
+
+from moments.LD import Numerics
+
+import copy
+import moments
+from moments.Misc import perturb_params
+from moments.Misc import delayed_flush
+
+from scipy.special import gammaln
+import scipy.optimize
+
+"""
+Adapted from moments/dadi to infer input parameters of demographic model
+Usage is the same as moments.Inference, but inference using LD statistics 
+requires a bit more for inputs
+There are two options: run inference with LD stats alone, or LD+AFS
+If we are using LD stats alone, data = [means, varcovs], a list of statistics 
+means and the bootstrapped variance-covariance matrix
+If we use LD+AFS, data = [means, varcovs, fs]
+To use the frequency spectrum in the inference, we set the flag use_afs=True
+
+"""
+
+_counter = 0
+
+def multivariate_normal_pdf(x,mu,Sigma):
+    p = len(x)
+    return np.sqrt(np.linalg.det(Sigma)/(2*math.pi)**p) * np.exp( -1./2 * 
+                        np.dot( np.dot( (x-mu).transpose() , 
+                                np.linalg.inv(Sigma) ) , x-mu ) )
+
+def ll(x,mu,Sigma):
+    """
+    x = data
+    mu = model function output
+    Sigma = variance-covariance matrix
+    """
+    return -1./2 * np.dot( np.dot( (x-mu).transpose() , 
+                            np.linalg.inv(Sigma) ) , x-mu ) 
+                            #- len(x)*np.pi - 1./2*np.log(np.linalg.det(Sigma)) 
+
+def ll_over_bins(xs,mus,Sigmas):
+    """
+    xs = list of data arrays
+    mus = list of model function output arrays
+    Sigmas = list of var-cov matrices
+    Lists must be in the same order
+    Each bin is assumed to be independent, so we call ll(x,mu,Sigma) 
+      for each bin
+    """
+    it = iter([xs,mus,Sigmas])
+    the_len = len(next(it))
+    if not all(len(l) == the_len for l in it):
+        raise ValueError('Lists of data, means, and varcov matrices must be the same length')
+    ll_vals = []
+    for ii in range(len(xs)):
+        ll_vals.append(ll(xs[ii],mus[ii],Sigmas[ii]))
+    ll_val = np.sum(ll_vals)
+    return ll_val
+
+"""(model_func, ms, vcs, fs, 
+            rhos, rs, 
+            theta, u, Ne, 
+            lower_bound, upper_bound, 
+            verbose, flush_delay,
+            func_args, func_kwargs, fixed_params, 
+            use_afs, Leff, multinom, ns,
+            output_stream)"""
+
+_out_of_bounds_val = -1e12
+def _object_func(params, model_func, means, varcovs, fs=None,
+                 rhos=None, rs = None,
+                 theta=None, u=None, Ne=None,
+                 lower_bound=None, upper_bound=None,
+                 verbose=0, flush_delay=0, 
+                 func_args=[], func_kwargs={}, fixed_params=None,
+                 use_afs=False, Leff=None, multinom=True, ns=None,
+                 output_stream=sys.stdout):
+    global _counter
+    _counter += 1
+    
+    # Deal with fixed parameters
+    params_up = _project_params_up(params, fixed_params)
+    
+    # Check our parameter bounds
+    if lower_bound is not None:
+        for pval,bound in zip(params_up, lower_bound):
+            if bound is not None and pval < bound:
+                return -_out_of_bounds_val
+    if upper_bound is not None:
+        for pval,bound in zip(params_up, upper_bound):
+            if bound is not None and pval > bound:
+                return -_out_of_bounds_val
+    
+    all_args = [params_up] + list(func_args)
+    
+    if theta is None:
+        if Ne is None:
+            Ne = params_up[-1]
+            theta = 4*Ne*u
+            if rhos is None:
+                rhos = [4*Ne*r for r in rs]
+            all_args = [all_args[0][:-1]]
+        else:
+            theta = 4*Ne*u
+            if rhos is None:
+                rhos = [4*Ne*r for r in rs]
+    else:
+        if rhos is None:
+            if Ne is not None:
+                rhos = [4*Ne*r for r in rs]
+        
+    ## first get ll of afs
+    if use_afs == True:
+        if Leff is None:
+            model = theta * model_func[1](all_args[0],ns)
+        else:
+            model = Leff * theta * model_func[1](all_args[0],ns)
+        if fs.folded:
+            model = model.fold()
+        if multinom == True:
+            ll_afs = moments.Inference.ll_multinom(model,fs)
+        else:
+            ll_afs = moments.Inference.ll(model,fs)
+    
+    ## next get ll for LD stats
+    ## we use simpson's rule to integrate over bins, so we also pass the midpoints of each rho bin
+    rhos = np.array(rhos)
+    mid_rhos = (rhos[:-1] + rhos[1:])/2.
+    
+    pass_rhos = np.empty(len(rhos)+len(mid_rhos))
+    for ii in range(len(mid_rhos)):
+        pass_rhos[2*ii] = rhos[ii]
+        pass_rhos[2*ii+1] = mid_rhos[ii]
+    
+    pass_rhos[-1] = rhos[-1]
+    
+    func_kwargs = {'theta':theta, 'rho':pass_rhos}
+        
+    # compute ld statistics for the passed parameters, and given theta and rhos
+    stats = model_func[0](*all_args, **func_kwargs)
+    ld_stats = stats[:-1]
+    het_stats = stats[-1]
+    
+    simp_stats = []
+    for ii in range(len(rhos)-1):
+        simp_stats.append((ld_stats[2*ii] + 4*ld_stats[2*ii+1] + ld_stats[2*ii+2])/6.)
+    
+    if use_afs == False:
+        simp_stats.append(het_stats)
+    
+    ## resulting ll from afs (if used) plus ll from rho bins
+    if use_afs == True:
+        result = ll_afs + ll_over_bins(means, simp_stats, varcovs)
+    else:
+        result = ll_over_bins(means, simp_stats, varcovs)
+    
+    # Bad result
+    if np.isnan(result):
+        print("got bad results...")
+        result = _out_of_bounds_val
+        
+    if (verbose > 0) and (_counter % verbose == 0):
+        param_str = 'array([%s])' % (', '.join(['%- 12g'%v for v in params_up]))
+        output_stream.write('%-8i, %-12g, %s%s' % (_counter, result, param_str,
+                                                   os.linesep))
+        delayed_flush(delay=flush_delay)
+    
+    return -result
+
+def _object_func_log(log_params, *args, **kwargs):
+    return _object_func(np.exp(log_params), *args, **kwargs)
+
+def optimize_log_fmin(p0, data, model_func,
+                 rhos=None, rs=None,
+                 theta=None, u=None, Ne=None, 
+                 lower_bound=None, upper_bound=None, 
+                 verbose=0, flush_delay=0.5,
+                 func_args=[], func_kwargs={}, fixed_params=None, 
+                 use_afs=False, Leff=None, multinom=False, ns=None):
+    """
+    p0 : initial guess (demography parameters + theta)
+    data : [means, varcovs, fs (optional, use if use_afs=True)]
+    means : list of mean statistics matching bins (has length len(rhos)-1)
+    varcovs : list of varcov matrices matching means
+    model_func : demographic model to compute statistics for a given rho
+                 If we are using AFS, it's a list of the two models [LD, AFS]
+                 If it's LD stats alone, it's just a single LD model (still passed as a list)
+    rhos : pass fixed list of rho bin edges (4*Ne*r)
+    rs : list of raw recombination rates, to be scaled by Ne (either passed or last value in list of params)
+    theta : this is population scaled per base mutation rate (4*Ne*mu, not 4*Ne*mu*L)
+    u : raw per base mutation rate, theta found by 4*Ne*u
+    Ne : pass if we want a fixed effective population size to scale u and r (if theta and rhos aren't fixed)
+    lower_bound : 
+    upper_bound : 
+    verbose : 
+    flush_delay : 
+    func_args : 
+    func_kwargs : 
+    fixed_params : 
+    use_afs : we pass a model to compute the frequency spectrum and use that instead of heterozygosity statistics
+    Leff : effective length of genome from which the fs was generated (only used if fitting to afs)
+    multinom : only relevant if we are using the AFS, likelihood computed for scaled FS 
+               vs fixed scale of FS from theta and Leff
+    ns : sample size (only needed if we are using the frequency spectrum, as we ns does not affect mean LD stats) 
+    
+    We can either pass a fixed mutation rate theta = 4*N*u, or we pass u and Ne (and compute theta),
+        or we pass u and Ne is a parameter of our model to fit (which scales both the mutation rate and
+        the recombination rates).
+    We can either pass fixed rho values for the bins, or we pass r and Ne, or we pass r and Ne is a 
+        parameter of our model, just as for the mutation rate.
+    """
+    output_stream = sys.stdout
+    
+    means = data[0]
+    varcovs = data[1]
+    if use_afs == True:
+        try:
+            fs = data[2]
+        except IndexError:
+            raise ValueError("if use_afs=True, need to pass frequency spectrum in data=[means,varcovs,fs]")
+        
+        if ns == None:
+            raise ValueError("need to set ns if we are fitting frequency spectrum")
+        
+    else:
+        fs = None
+        
+    if use_afs == True:
+        raise ValueError("which mutation/theta parameters do we need to check and pass")
+    
+    if rhos is None and rs is None:
+        raise ValueError("need to pass one of rhos or rs")
+    
+    if rhos is None and Ne is None:
+        print("using last parameter is list of params as Ne")
+    
+    ms = copy.copy(means)
+    vcs = copy.copy(varcovs)
+    
+    args = (model_func, ms, vcs, fs, 
+            rhos, rs, 
+            theta, u, Ne, 
+            lower_bound, upper_bound, 
+            verbose, flush_delay,
+            func_args, func_kwargs, fixed_params, 
+            use_afs, Leff, multinom, ns,
+            output_stream)
+    
+    p0 = _project_params_down(p0, fixed_params)
+    outputs = scipy.optimize.fmin(_object_func_log, np.log(p0), args=args, full_output=True, disp=False)
+    
+    xopt, fopt, iter, funcalls, warnflag = outputs
+    xopt = _project_params_up(np.exp(xopt), fixed_params)
+    
+    return xopt, fopt
+
+def optimize_log_powell(p0, data, model_func,
+                 rhos=None, rs=None,
+                 theta=None, u=None, Ne=None, 
+                 lower_bound=None, upper_bound=None, 
+                 verbose=0, flush_delay=0.5,
+                 func_args=[], func_kwargs={}, fixed_params=None, 
+                 use_afs=False, Leff=None, multinom=False, ns=None):
+    """
+    p0 : initial guess (demography parameters + theta)
+    data : [means, varcovs, fs (optional, use if use_afs=True)]
+    means : list of mean statistics matching bins (has length len(rhos)-1)
+    varcovs : list of varcov matrices matching means
+    model_func : demographic model to compute statistics for a given rho
+                 If we are using AFS, it's a list of the two models [LD, AFS]
+                 If it's LD stats alone, it's just a single LD model (still passed as a list)
+    rhos : pass fixed list of rho bin edges (4*Ne*r)
+    rs : list of raw recombination rates, to be scaled by Ne (either passed or last value in list of params)
+    theta : this is population scaled per base mutation rate (4*Ne*mu, not 4*Ne*mu*L)
+    u : raw per base mutation rate, theta found by 4*Ne*u
+    Ne : pass if we want a fixed effective population size to scale u and r (if theta and rhos aren't fixed)
+    lower_bound : 
+    upper_bound : 
+    verbose : 
+    flush_delay : 
+    func_args : 
+    func_kwargs : 
+    fixed_params : 
+    use_afs : we pass a model to compute the frequency spectrum and use that instead of heterozygosity statistics
+    Leff : effective length of genome from which the fs was generated (only used if fitting to afs)
+    multinom : only relevant if we are using the AFS, likelihood computed for scaled FS 
+               vs fixed scale of FS from theta and Leff
+    ns : sample size (only needed if we are using the frequency spectrum, as we ns does not affect mean LD stats) 
+    
+    We can either pass a fixed mutation rate theta = 4*N*u, or we pass u and Ne (and compute theta),
+        or we pass u and Ne is a parameter of our model to fit (which scales both the mutation rate and
+        the recombination rates).
+    We can either pass fixed rho values for the bins, or we pass r and Ne, or we pass r and Ne is a 
+        parameter of our model, just as for the mutation rate.
+    """
+    output_stream = sys.stdout
+    
+    means = data[0]
+    varcovs = data[1]
+    if use_afs == True:
+        try:
+            fs = data[2]
+        except IndexError:
+            raise ValueError("if use_afs=True, need to pass frequency spectrum in data=[means,varcovs,fs]")
+        
+        if ns == None:
+            raise ValueError("need to set ns if we are fitting frequency spectrum")
+        
+    else:
+        fs = None
+        
+    if use_afs == True:
+        raise ValueError("which mutation/theta parameters do we need to check and pass")
+    
+    if rhos is None and rs is None:
+        raise ValueError("need to pass one of rhos or rs")
+    
+    if rhos is None and Ne is None:
+        print("using last parameter is list of params as Ne")
+    
+    ms = copy.copy(means)
+    vcs = copy.copy(varcovs)
+
+    args = (model_func, ms, vcs, fs, 
+            rhos, rs, 
+            theta, u, Ne, 
+            lower_bound, upper_bound, 
+            verbose, flush_delay,
+            func_args, func_kwargs, fixed_params, 
+            use_afs, Leff, multinom, ns,
+            output_stream)
+        
+    p0 = _project_params_down(p0, fixed_params)
+    outputs = scipy.optimize.fmin_powell(_object_func_log, np.log(p0), args=args, full_output=True, disp=False)
+    
+    xopt, fopt, direc, iter, funcalls, warnflag = outputs
+    xopt = _project_params_up(np.exp(xopt), fixed_params)
+    
+    return xopt, fopt
+
+
+def _project_params_down(pin, fixed_params):
+    """
+    Eliminate fixed parameters from pin.
+    """
+    if fixed_params is None:
+        return pin
+
+    if len(pin) != len(fixed_params):
+        raise ValueError('fixed_params list must have same length as input '
+                         'parameter array.')
+
+    pout = []
+    for ii, (curr_val,fixed_val) in enumerate(zip(pin, fixed_params)):
+        if fixed_val is None:
+            pout.append(curr_val)
+
+    return np.array(pout)
+
+def _project_params_up(pin, fixed_params):
+    """
+    Fold fixed parameters into pin.
+    """
+    if fixed_params is None:
+        return pin
+
+    if np.isscalar(pin):
+        pin = [pin]
+
+    pout = np.zeros(len(fixed_params))
+    orig_ii = 0
+    for out_ii, val in enumerate(fixed_params):
+        if val is None:
+            pout[out_ii] = pin[orig_ii]
+            orig_ii += 1
+        else:
+            pout[out_ii] = fixed_params[out_ii]
+    return pout
+
+
