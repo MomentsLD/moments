c8b36c77224c8711167cdec709a48cae92150ff2
Aaron Ragsdale <aragsdale@math.arizona.edu>
2019-09-10 17:01:23 -0400

    minor change to two locus demographics function parameter handling

--- moments/TwoLocus/Demographics.py
+++ moments/TwoLocus/Demographics.py
@@ -27,10 +27,24 @@ def set_cache_path(path='~/.moments/TwoLocus_cache'):
 cache_path=None
 set_cache_path()
 
+def make_floats(params):
+    """
+    pass list of params, return floats of those params (for caching)
+    """
+    if params is None:
+        return None
+    if hasattr(params, "__len__"):
+        return [float(p) for p in params]
+    else:
+        return float(params)
 
 def equilibrium(ns, rho=None, theta=1.0, gamma=None, h=0.5, sel_params=None):
     """
     Compute or load the equilibrium two locus frequency spectrum
+    gamma and h, if set, are only for selection at the A/a locus
+    sel_params, which are the (additive) selection coefficients for haplotypes AB, Ab, and aB
+        so that sel_params = (sAB, sA, sB)
+        If sAB = sA + sB, no epistatic interaction
     """
     if rho == None:
         print("Warning: no rho value set. Simulating with rho = 0.")
@@ -39,15 +53,30 @@ def equilibrium(ns, rho=None, theta=1.0, gamma=None, h=0.5, sel_params=None):
     if gamma==None:
         gamma=0.0
     
+    gamma = make_floats(gamma)
+    rho = make_floats(rho)
+    theta = make_floats(theta)
+    sel_params = make_floats(sel_params)
+    
     # fetch from cache if neutral (cache only neutral spectra for the moment)
-    eq_name = 'tlfs_ns{0}_rho{1}_theta{2}.fs'.format(ns,rho,theta)
-    eq_name = os.path.join(cache_path, eq_name)
+    if gamma == 0.0 and (sel_params == None or (sel_params[0] == 0.0 and sel_params[1] == 0.0 and sel_params[2] == 0.0)):
+        eq_name = 'tlfs_ns{0}_rho{1}_theta{2}.fs'.format(ns,rho,theta)
+        eq_name = os.path.join(cache_path, eq_name)
+    elif gamma != 0.0: # cache for gammaA, hA
+        eq_name = 'tlfs_ns{0}_rho{1}_theta{2}_gammaA{3}_hA{4}.fs'.format(ns,rho,theta,gamma,h)
+        eq_name = os.path.join(cache_path, eq_name)
+    elif sel_params != None:
+        eq_name = 'tlfs_ns{0}_rho{1}_theta{2}_sel_{3}_{4}_{5}.fs'.format(ns,rho,theta,sel_params[0],sel_params[1],sel_params[2])
+        eq_name = os.path.join(cache_path, eq_name)
+    
     try:
         F = moments.TwoLocus.TLSpectrum.from_file(eq_name)
     except IOError:
         F = np.zeros((ns+1,ns+1,ns+1))
+        ### I would rather compute the equilibrium state by inverting the transition matrix
+        ### so long as it is not poory conditioned (note 7/16)
         F = moments.TwoLocus.Integration.integrate(F, 1.0, 40.0, rho=rho, theta=theta, 
-                                gamma=gamma, h=h, sel_params=sel_params)
+                                gamma=gamma, h=h, sel_params=sel_params, dt=0.001)
         F = moments.TwoLocus.TLSpectrum(F)
         F.to_file(eq_name)
     return F
@@ -65,6 +94,11 @@ def two_epoch(params, ns, rho=None, theta=1.0, gamma=None, h=0.5, sel_params=Non
     
     if gamma==None:
         gamma=0.0
+
+    gamma = make_floats(gamma)
+    rho = make_floats(rho)
+    theta = make_floats(theta)
+    sel_params = make_floats(sel_params)
     
     F = equilibrium(ns, rho=rho, theta=theta, gamma=gamma, h=h, sel_params=sel_params)
     F.integrate(nu, T, rho=rho, theta=theta, 
@@ -85,6 +119,11 @@ def three_epoch(params, ns, rho=None, theta=1.0, gamma=None, h=0.5, sel_params=N
     if gamma==None:
         gamma=0.0
     
+    gamma = make_floats(gamma)
+    rho = make_floats(rho)
+    theta = make_floats(theta)
+    sel_params = make_floats(sel_params)
+
     F = equilibrium(ns, rho=rho, theta=theta, gamma=gamma, h=h, sel_params=sel_params)
     F.integrate(nu1, T1, rho=rho, theta=theta, 
                 gamma=gamma, h=h, sel_params=sel_params)
@@ -107,6 +146,11 @@ def growth(params, ns, rho=None, theta=1.0, gamma=None, h=0.5, sel_params=None):
     if gamma==None:
         gamma=0.0
 
+    gamma = make_floats(gamma)
+    rho = make_floats(rho)
+    theta = make_floats(theta)
+    sel_params = make_floats(sel_params)
+
     F = equilibrium(ns, rho=rho, theta=theta, gamma=gamma, h=h, sel_params=sel_params)
     nu_func = lambda t: np.exp(np.log(nu) * t/T)
     F.integrate(nu_func, T, rho=rho, theta=theta, gamma=gamma, h=h, sel_params=sel_params)
@@ -127,6 +171,11 @@ def bottlegrowth(params, ns, rho=None, theta=1.0, gamma=None, h=0.5, sel_params=
     
     if gamma==None:
         gamma=0.0
+
+    gamma = make_floats(gamma)
+    rho = make_floats(rho)
+    theta = make_floats(theta)
+    sel_params = make_floats(sel_params)
 
     F = equilibrium(ns, rho=rho, theta=theta, gamma=gamma, h=h, sel_params=sel_params)
     nu_func = lambda t: nuB * np.exp(np.log(nuF/nuB) * t/T)
